f65e0a7009c2fdbee05da5bbec29c74ba65fe4dc
Conor-Hall940 <conorlhall0607@gmail.com>
2026-04-12 17:59:07 -0400

    Update lanuage1.cxx

--- Projects/language1/lanuage1.cxx
+++ Projects/language1/lanuage1.cxx
@@ -12,3 +12,24 @@ C++
 
   return0;
   } 
+
+#include <iostream>
+
+//corrected code 
+int main() {
+    // 1. Declare variables with types (double allows for decimals)
+    double length, width, area;
+
+    std::cout << "Enter the length of the rectangle: ";
+    std::cin >> length;
+
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    // 2. Perform the calculation
+    area = length * width;
+
+    std::cout << "The area of the rectangle is: " << area << std::endl;
+
+    return 0;
+}
